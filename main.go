ffffb9f6c6bf87194faad86205ec39eb6ddd2a43
NMishchenko <mishchenko.nls@gmail.com>
2022-11-16 20:39:40 +0200

    Implement basic functionality

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strings"
+)
+
+const sortingFieldIndex = 0
 
 func main() {
-    fmt.Println("hello world")
+	var content string
+	content = ReadFromConsole()
+
+	fmt.Println("Sorted data:\n" + content)
+}
+
+func ReadFromConsole() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	return StartProcessing(scanner)
+}
+
+func StartProcessing(scanner *bufio.Scanner) string {
+	n := 0
+	table := [][]string{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		row := strings.Split(line, ",")
+
+		table = append(table, row)
+	}
+
+	sort.Slice(table, func(i, j int) bool {
+		return table[i][sortingFieldIndex] < table[j][sortingFieldIndex]
+	})
+
+	var result strings.Builder
+
+	for _, row := range table {
+		result.WriteString(strings.Join(row, ","))
+		result.WriteString("\n")
+	}
+
+	return result.String()
 }
